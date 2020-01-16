e9a420622d3cd3de0602e4a727cbe4ce77bef485
Mauro Guardarini <mauro.guardarini@gmail.com>
2020-01-16 00:43:51 -0300

    adicionado controller e rota get para listar os devs

--- src/routes.js
+++ src/routes.js
@@ -2,32 +2,10 @@ const { Router } = require("express");
 const axios = require("axios");
 const Dev = require("./models/Dev.js")
 const routes = Router();
+const DevController = require("./controllers/DevController.js");
 
-routes.post("/devs", async (request,response)=>{
+routes.post("/devs", DevController.store);
 
-   const { github_username, techs, latitude, longitude } = request.body;
-
-   const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-   
-   const { name, avatar_url, bio } = apiResponse;
-
-   const techsArray = techs.split(',').map(tech=>tech.trim());
-
-   const dev = await Dev.create({
-       github_username,
-       name,
-       avatar_url,
-       bio,
-       techs: techsArray
-   })
-
-   return response.json(dev); 
-
-
-});
-
-routes.get("/",(request,response)=>{
-   return response.json({message:"Hello World"}); 
-});
+routes.get("/devs",DevController.index);
 
 module.exports = routes;
