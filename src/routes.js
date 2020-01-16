2dab219eef954feff3ab9fa928ddae0723793ca9
Mauro Guardarini <mauro.guardarini@gmail.com>
2020-01-16 01:07:29 -0300

    adicionada controller para buscar usuario

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const axios = require("axios");
 const Dev = require("./models/Dev.js")
 const routes = Router();
 const DevController = require("./controllers/DevController.js");
+const SearchController = require("./controllers/SearchController.js");
 
 routes.post("/devs", DevController.store);
 
 routes.get("/devs",DevController.index);
+
+routes.get("/search",SearchController.index);
 
 module.exports = routes;
